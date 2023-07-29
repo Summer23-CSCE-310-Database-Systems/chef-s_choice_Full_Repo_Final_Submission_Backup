0edc770873b5304c78f4f9e4792abd76cc81bf58
BuildTools <unconfigured@null.spigotmc.org>
2023-07-28 21:22:22 -0500

    connected

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  // Define a function to fetch data from the backend API
+  const fetchDataFromBackend = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/users'); // Make a GET request to your backend API
+      const data = await response.json(); // Parse the response as JSON
+      console.log(data); // Output the fetched data to the console (you can update this to update your UI with the data)
+    } catch (error) {
+      console.error('Error fetching data from the backend:', error);
+    }
+  };
+
+  // Call the fetch function when the component mounts or when you want to trigger the request
+  // For example, you can call it inside a button click event, useEffect, or any other event.
+  fetchDataFromBackend();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
