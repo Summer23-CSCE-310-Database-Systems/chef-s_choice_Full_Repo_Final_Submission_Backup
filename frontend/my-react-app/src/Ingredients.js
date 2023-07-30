fdb1873845d9d601d2fbb0df00776b8906631b6a
SamnBierle <samuelbierle@gmail.com>
2023-07-30 14:05:07 -0500

    Committing Edits to Ingredients Page made by Citlali

--- frontend/my-react-app/src/Ingredients.js
+++ frontend/my-react-app/src/Ingredients.js
@@ -3,11 +3,97 @@ import './Ingredients.css';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-function Ingredients() {
-    return (
+// Use the absolute URL of the backend server
+const backendURL = 'http://localhost:5200/backend/api/ingredients';
 
-        "ingredients"
-    );
-}
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [id, setId] = useState(null);
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [caloriesPerOz, setCaloriesPerOz] = useState(0);
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios.get(backendURL).then((response) => {
+      setIngredients(response.data);
+    });
+  }, []);
+
+  const handleAddIngredient = () => {
+    axios.post(backendURL, {
+      name: name,
+      details: details,
+      calories_per_oz: caloriesPerOz,
+    }).then((response) => {
+      setIngredients([...ingredients, response.data]);
+    });
+  };
+
+  const handleEditIngredient = () => {
+    axios.put(backendURL + '/' + id, {
+      name: name,
+      details: details,
+      calories_per_oz: caloriesPerOz,
+    }).then((response) => {
+      setIngredients([...ingredients, response.data]);
+    });
+  };
+
+  const handleDeleteIngredient = () => {
+    axios.delete(backendURL + '/' + id).then((response) => {
+      setIngredients(ingredients.filter((ingredient) => ingredient.id !== id));
+    });
+  };
+
+  const viewIngredient = (ingredient) => {
+    setName(ingredient.name);
+    setDetails(ingredient.details);
+    setCaloriesPerOz(ingredient.calories_per_oz);
+    setId(ingredient.id);
+  };
+
+  return (
+    <ingredients>
+    <div>
+      <h1>Ingredients</h1>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient.id}>
+            {ingredient.name}
+            <button onClick={() => viewIngredient(ingredient)}>View</button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleAddIngredient}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Details"
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Calories per oz"
+          value={caloriesPerOz}
+          onChange={(e) => setCaloriesPerOz(e.target.value)}
+        />
+        <button type="submit">Add Ingredient</button>
+      </form>
+
+      <div>
+        <button onClick={() => navigate('/create')}>Create Ingredient</button>
+        <button onClick={() => navigate('/edit')}>Edit Ingredient</button>
+        <button onClick={() => navigate('/delete')}>Delete Ingredient</button>
+      </div>
+    </div>
+        </ingredients>
+  );
+};
 
 export default Ingredients;
